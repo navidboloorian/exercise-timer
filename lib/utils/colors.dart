c0372b56ab637a759d7bbc373b7e58357c71d539
Navid Boloorian <navidboloorian0@gmail.com>
2022-09-04 16:16:22 -0700

    added basic dark theme

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  static const Color darkBackground = Color(0xff686868);
+  static const Color darkText = Color(0xffffffff);
+}
---
+++ lib/utils/themes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class Themes {
+  // dark theme
+  static ThemeData get dark {
+    return ThemeData(
+      primaryColor: CustomColors.darkBackground,
+      scaffoldBackgroundColor: CustomColors.darkBackground,
+      fontFamily: 'Montserrat',
+      appBarTheme: const AppBarTheme(
+        backgroundColor: CustomColors.darkBackground,
+        foregroundColor: CustomColors.darkText,
+        centerTitle: true,
+        elevation: 0,
+        titleTextStyle: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 50,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+}
